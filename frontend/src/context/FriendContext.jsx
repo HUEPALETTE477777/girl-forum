63ed7e4f6fe7e78ee1dd0b14fdc697f5434a853c
kyle nguyen <ivanguan18@gmail.com>
2025-08-23 19:25:46 -0700

    ASdasdsasda

--- frontend/src/context/FriendContext.jsx
+++ frontend/src/context/FriendContext.jsx
@@ -107,6 +107,8 @@ export const FriendProvider = ({ children }) => {
 
     const fetchTargetUserFriends = async (userId) => {
         try {
+            setTargetUserFriends([]);
+
             const res = await axios.get(`${import.meta.env.VITE_BACKEND_ENDPOINT}/api/friends/${userId}`, {
                 withCredentials: true,
             })
